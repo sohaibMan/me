3a2a0f6e719bd607efbb75db2484670b118dcb31
SohaibMan <96356071+sohaibMan@users.noreply.github.com>
2022-06-28 05:58:39 +0100

    update

---
+++ modal.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnContinueModal = document.querySelector('.continue');
+
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+
+btnCloseModal.addEventListener('click', closeModal);
+btnContinueModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+  
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+const wstosec = (min) => {return min*60 * 1000};//convert seconds to milliseconds
+
+const form = document.querySelector(".input-group");
+form.addEventListener("submit", function () {
+closeModal();
+})
+
+
+window.setTimeout(openModal, wstosec(.05));///nos d9i9a o taban dak pop f site dyali 
+
+    
