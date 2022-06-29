768a928675893981d3cf5219518ebf2752b3d289
SohaibMan <96356071+sohaibMan@users.noreply.github.com>
2022-06-29 21:42:40 +0100

    update' git commit -m update; git commit -m update git commit -m "update";
    git commit -m "update

---
+++ src/js/tranlate.js
@@ -0,0 +1,11 @@
+
+
+	function googleTranslateElementInit() {
+        new google.translate.TranslateElement(
+            {pageLanguage: 'en'},
+            'google_translate_element'
+        );
+    }
+
+//const select_option=document.querySelector('.goog-te-combo');// i am gonna keep french and arab support only
+
