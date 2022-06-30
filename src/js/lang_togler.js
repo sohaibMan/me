b0c7efedc33151487ad576be4d5c5a849ac3a55d
SohaibMan <96356071+sohaibMan@users.noreply.github.com>
2022-06-30 20:56:44 +0100

    udapte

--- src/js/lang_togler.js
+++ src/js/lang_togler.js
@@ -4,6 +4,14 @@ let is_default=true;
 const img=document.querySelector(".img--1");
 const imgsm=document.querySelector(".img--2");
 const download_button=document.querySelector(".download--anchor");
+const  download_icon=document.querySelector(".em");
+const  text=document.querySelector(".text_downlaod");
+const  textoldvalue=text.textContent;//test of manules trudction that will be make my website slower and make 
+//a lot of work for the developer and mainting the website (those varibles will be in the stack memory ues objects ,,,,,)
+
+
+
+
 
 
 let lang=localStorage.getItem('lang');
@@ -18,18 +26,25 @@ function switchtofr(){
   img.src=img.src.replace(/eng/g,'fr');
   imgsm.src=imgsm.src.replace(/eng/g,'fr');
   download_button.href=download_button.href.replace(/eng/g,'fr');
+  download_icon.classList.add('em-fr');
+  download_icon.classList.remove('em-flag-um');
+  text.textContent="Telecharger au format pdf"; 
+  
   
 }
-
 function backtoeng(){
     
     localStorage.setItem('lang','ang');
     img.src=img.src.replace(/fr/g,'eng');
     imgsm.src=imgsm.src.replace(/fr/g,'eng');
     download_button.href=download_button.href.replace(/fr/g,'eng');
+  
 
-   
+    download_icon.classList.add('em-flag-um');
+    download_icon.classList.remove('em-fr');
+    text.textContent=textoldvalue;; 
 }
+
 
 LangTogller.addEventListener('change', ()=>{
     
